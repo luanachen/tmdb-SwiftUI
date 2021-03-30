28fe292d43803e463096736cf4e2281a6bd435f6
Luana Chen <luanachen@gmail.com>
2021-03-30 18:29:13 -0300

    initial commit

---
+++ MoviesDB-Luana/MoviesDB_LuanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesDB_LuanaApp.swift
+//  MoviesDB-Luana
+//
+//  Created by Luana Chen Chih Jun on 30/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MoviesDB_LuanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
