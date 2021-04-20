933428403d51d99f875cb8f380cda32334a737eb
Luana <fvd_ljun@uolinc.com>
2021-04-20 15:09:27 -0300

    Adds LoginServiceTests

---
+++ MoviesDB-LuanaTests/Login/Service/LoginServiceTests.swift
@@ -0,0 +1,38 @@
+//
+//  LoginServiceTests.swift
+//  MoviesDB-LuanaTests
+//
+//  Created by Luana Chen Chih Jun on 19/04/21.
+//
+
+import Foundation
+import XCTest
+@testable import MoviesDB_Luana
+
+class LoginServiceTests: XCTestCase {
+
+    var sut: LoginService!
+    var sessionManagerMock: SessionStoreManagerMock!
+
+    override func setUp() {
+        super.setUp()
+        sessionManagerMock = SessionStoreManagerMock()
+        sut = LoginService(sessionStoreManager: sessionManagerMock)
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func test_saveUserSession_callsSessionManagerSave() {
+        let sessionId = "123"
+        sut.saveUserSession(sessionId: sessionId)
+        XCTAssertEqual(sessionManagerMock.data, Data(from: sessionId))
+    }
+
+    func test_getUserSession_callsSessionManagerLoad() {
+        XCTAssertEqual(sut.getUserSession(), "123")
+    }
+
+}
---
+++ MoviesDB-LuanaTests/Mocks/SessionStoreManagerMock.swift
@@ -0,0 +1,26 @@
+//
+//  SessionStoreManagerMock.swift
+//  MoviesDB-LuanaTests
+//
+//  Created by Luana Chen Chih Jun on 19/04/21.
+//
+
+import Foundation
+@testable import MoviesDB_Luana
+
+class SessionStoreManagerMock: SessionStoreManagerType {
+    var didCallSave = false
+    var didCallLoad = false
+    var data = Data()
+
+    func save(key: String, data: Data) -> OSStatus {
+        didCallSave = true
+        self.data = data
+        return OSStatus()
+    }
+
+    func load(key: String) -> Data? {
+        didCallLoad = true
+        return Data(from: "123")
+    }
+}
